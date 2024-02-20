62e3d48be49f44fc255bb2899c9c9e6cf67bc798
Nik <Nik96.ge@gmail.com>
2024-02-20 17:24:41 +0330

    Add dev server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    server: {
+      proxy: {
+        "/api/": {
+          target: env.VITE_DEV_SERVER,
+          changeOrigin: true
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
+    plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+    }
+  };
 });
