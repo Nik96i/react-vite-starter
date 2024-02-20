6ccbf7e106034d433db1999e13e6e70518aff57d
Nik <Nik96.ge@gmail.com>
2024-02-20 17:30:24 +0330

    Update alias according to the Vue starter

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,7 +18,10 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [react()],
     resolve: {
-      alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+      // alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
     }
   };
 });
