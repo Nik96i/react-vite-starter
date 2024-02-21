20b2f4413254227603dae620b204596ad4116cd3
Nik <Nik96.ge@gmail.com>
2024-02-21 18:41:15 +0330

    Add JavaScript rules.

---
+++ lints/javascript/index.js
@@ -0,0 +1,114 @@
+export default {
+
+  // Enforce return statements in callbacks of array methods
+  // https://eslint.org/docs/latest/rules/array-callback-return
+  "array-callback-return": "error",
+
+  // Disallow await inside of loops
+  // https://eslint.org/docs/latest/rules/no-await-in-loop
+  "no-await-in-loop": "off",
+
+  // Disallow expressions where the operation doesn't affect the value
+  // https://eslint.org/docs/latest/rules/no-constant-binary-expression
+  "no-constant-binary-expression": "error",
+
+  // Disallow returning value from constructor
+  // https://eslint.org/docs/latest/rules/no-constructor-return
+  "no-constructor-return": "error",
+
+  // Disallow duplicate module imports
+  // https://eslint.org/docs/latest/rules/no-duplicate-imports
+  "no-duplicate-imports": "error",
+
+  // Disallow new operators with global non-constructor functions
+  // https://eslint.org/docs/latest/rules/no-new-native-nonconstructor
+  "no-new-native-nonconstructor": "error",
+
+  // Disallow returning values from Promise executor functions
+  // https://eslint.org/docs/latest/rules/no-promise-executor-return
+  "no-promise-executor-return": "warn",
+
+  // Disallow comparisons where both sides are exactly the same
+  // https://eslint.org/docs/latest/rules/no-self-compare
+  "no-self-compare": "error",
+
+  // Disallow template literal placeholder syntax in regular strings
+  // https://eslint.org/docs/latest/rules/no-template-curly-in-string
+  "no-template-curly-in-string": "off",
+
+  // Disallow unmodified loop conditions
+  // https://eslint.org/docs/latest/rules/no-unmodified-loop-condition
+  "no-unmodified-loop-condition": "off",
+
+  // Disallow loops with a body that allows only one iteration
+  // https://eslint.org/docs/latest/rules/no-unreachable-loop
+  "no-unreachable-loop": "warn",
+
+  // Disallow unused private class members
+  // https://eslint.org/docs/latest/rules/no-unused-private-class-members
+  "no-unused-private-class-members": "warn",
+
+  // Disallow the use of variables before they are defined
+  // https://eslint.org/docs/latest/rules/no-use-before-define
+  "no-use-before-define": "error",
+
+  // Disallow assignments that can lead to race conditions due to usage of await or yield
+  // https://eslint.org/docs/latest/rules/require-atomic-updates
+  "require-atomic-updates": "error",
+
+  // Enforce getter and setter pairs in objects and classes
+  // https://eslint.org/docs/latest/rules/accessor-pairs
+  "accessor-pairs": "off",
+
+  // Require braces around arrow function bodies
+  // https://eslint.org/docs/latest/rules/arrow-body-style
+  "arrow-body-style": ["error", "never"],
+
+  // Enforce the use of variables within the scope they are defined
+  // https://eslint.org/docs/latest/rules/block-scoped-var
+  "block-scoped-var": "error",
+
+  // Enforce camelcase naming convention
+  // https://eslint.org/docs/latest/rules/camelcase
+  "camelcase": "off",
+
+  // Enforce or disallow capitalization of the first letter of a comment
+  // https://eslint.org/docs/latest/rules/capitalized-comments
+  "capitalized-comments": "warn",
+
+  // Enforce that class methods utilize this
+  // https://eslint.org/docs/latest/rules/class-methods-use-this
+  "class-methods-use-this": "error",
+
+  // Enforce a maximum cyclomatic complexity allowed in a program
+  // https://eslint.org/docs/latest/rules/complexity
+  "complexity": "off",
+
+  // Require return statements to either always or never specify values
+  // https://eslint.org/docs/latest/rules/consistent-return
+  "consistent-return": "off",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
