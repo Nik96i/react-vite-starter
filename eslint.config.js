58f5ba0b825ff61aae2ea4efeac32a78987a1ab9
Nik <Nik96.ge@gmail.com>
2024-02-21 18:41:30 +0330

    Add Yarn lock.

--- eslint.config.js
+++ eslint.config.js
@@ -1,65 +1,69 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 // Plugins
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+import RRR from "./lints/stylistic/index.js";
 
 export default tseslint.config(
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    ignores: ['dist'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: './tsconfig.eslint.json',
+    "ignores": ["dist"],
+    "languageOptions": {
+      "ecmaVersion": "latest",
+      "parserOptions": {
+        "ecmaFeatures": {"jsx": true},
+        "project": "./tsconfig.eslint.json",
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        tsconfigRootDir: import.meta.dirname,
-      },
+        "tsconfigRootDir": import.meta.dirname
+      }
     },
-    plugins: {
-      react: react,
-      'react-hooks': reactHooks,
+    "plugins": {
+      "@stylistic": stylistic,
+      "react": react,
+      "react-hooks": reactHooks,
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      'react-refresh': reactRefresh,
+      "react-refresh": reactRefresh
     },
-    rules: {
-      ...react.configs.recommended.rules,
+    "rules": {
       ...reactHooks.configs.recommended.rules,
 
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      "react-refresh/only-export-components": ["warn", {"allowConstantExport": true}],
 
       // --- React Hooks
       // Enforce Rules of Hooks
-      'react-hooks/rules-of-hooks': 'error',
-      // Verify the list of the dependencies for Hooks like useEffect and similar
-      'react-hooks/exhaustive-deps': 'error',
+      "react-hooks/rules-of-hooks": "error",
 
-      '@typescript-eslint/strict-boolean-expressions': [
-        'error',
+      // Verify the list of the dependencies for Hooks like useEffect and similar
+      "react-hooks/exhaustive-deps": "error",
+
+      "@typescript-eslint/strict-boolean-expressions": [
+        "error",
         {
-          allowString: false,
-          allowNumber: false,
-          allowNullableObject: false,
-          allowNullableBoolean: false,
-          allowNullableString: false,
-          allowNullableNumber: false,
-          allowNullableEnum: false,
-          allowAny: false,
-          allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing: false,
-        },
+          "allowString": false,
+          "allowNumber": false,
+          "allowNullableObject": false,
+          "allowNullableBoolean": false,
+          "allowNullableString": false,
+          "allowNullableNumber": false,
+          "allowNullableEnum": false,
+          "allowAny": false,
+          "allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing": false
+        }
       ],
-    },
+
+      ...RRR
+
+    }
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
