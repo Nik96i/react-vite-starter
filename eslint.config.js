c80ff25a4aa055b77d72678370b0f91d045168f4
Nik <Nik96.ge@gmail.com>
2024-02-23 19:52:01 +0330

    Update eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,71 +1,82 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import stylistic from "@stylistic/eslint-plugin";
+import globals from "globals";
 
 // Plugins
+// import stylistic from "@stylistic/eslint-plugin";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-import RRR from "./lints/stylistic/index.js";
-
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
+
+  // React Plugin
   {
-    "ignores": ["dist"],
-    "languageOptions": {
-      "ecmaVersion": "latest",
-      "parserOptions": {
-        "ecmaFeatures": {"jsx": true},
-        "project": "./tsconfig.eslint.json",
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        "tsconfigRootDir": import.meta.dirname
-      }
-    },
-    "plugins": {
-      "@stylistic": stylistic,
-      "react": react,
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    plugins: {
+      react: react,
       "react-hooks": reactHooks,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       "react-refresh": reactRefresh
     },
-    "rules": {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
 
-      "react-refresh/only-export-components": ["warn", {"allowConstantExport": true}],
+      // Disable this rule because react 17 doesn't need to import React to use JSX
+      "react/react-in-jsx-scope": "off",
 
-      // --- React Hooks
       // Enforce Rules of Hooks
       "react-hooks/rules-of-hooks": "error",
-
       // Verify the list of the dependencies for Hooks like useEffect and similar
       "react-hooks/exhaustive-deps": "error",
-
-      "@typescript-eslint/strict-boolean-expressions": [
-        "error",
-        {
-          "allowString": false,
-          "allowNumber": false,
-          "allowNullableObject": false,
-          "allowNullableBoolean": false,
-          "allowNullableString": false,
-          "allowNullableNumber": false,
-          "allowNullableEnum": false,
-          "allowAny": false,
-          "allowRuleToRunWithoutStrictNullChecksIKnowWhatIAmDoing": false
-        }
-      ],
-
-      ...RRR
-
+      "react-refresh/only-export-components": ["warn", { "allowConstantExport": true }]
     }
   },
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: import.meta.dirname
+      }
+    },
+    rules: {
+      // TODO: Enable this
+      // Temporary disable this rule
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-argument": "off"
+    }
+  },
+
+  {
+    ignores: [
+      "dist",
+      "lints",
+      "tailwind.config.js",
+      "eslint.config.js",
+      "vite.config.ts",
+      "vite-env.d.ts",
+      "postcss.config.js"
+    ]
+  },
+
   eslintConfigPrettier
 );
