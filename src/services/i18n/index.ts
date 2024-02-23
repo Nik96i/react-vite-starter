afdee7a1cec70beebed930238673ba4a52c89835
Nik <Nik96.ge@gmail.com>
2024-02-23 16:48:53 +0330

    Remove i18n-http-backend

--- src/services/i18n/index.ts
+++ src/services/i18n/index.ts
@@ -1,16 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+
+// Import translation files
+import enTranslation from '@/services/i18n/locales/en/translation.json';
+import faTranslation from '@/services/i18n/locales/fa/translation.json';
 
 // Type Definitions
 export type SupportedLanguages = 'en' | 'fa';
 export type LocaleDirs = 'ltr' | 'rtl';
 
 void i18n
-  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   // .use(LanguageDetector)
@@ -19,6 +18,17 @@ void i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      fa: {
+        translation: faTranslation,
+      }
+    },
     debug: true,
     lng: 'fa',
     fallbackLng: 'fa',
